389fc94163aead9b61f8e6dd231bea2eb8ed0aeb
Ramsay Leung <ramsayleung@gmail.com>
2024-03-17 17:07:44 -0700

    Initial Commit

---
+++ iMeetup/iMeetupApp.swift
@@ -0,0 +1,17 @@
+//
+//  iMeetupApp.swift
+//  iMeetup
+//
+//  Created by ramsayleung on 2024-03-17.
+//
+
+import SwiftUI
+
+@main
+struct iMeetupApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
